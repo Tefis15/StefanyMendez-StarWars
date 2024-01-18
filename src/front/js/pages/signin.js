986deb281c2cec9af3339e97fad13d2c22fd3da4
Stefany María Méndez Salas <128415958+Tefis15@users.noreply.github.com>
2024-01-18 00:03:14 +0000

    [Add signin] --Add flux for signin

--- src/front/js/pages/signin.js
+++ src/front/js/pages/signin.js
@@ -18,74 +18,82 @@ const Signin = () => {
             <div className='text-center'>
                 <img className='img-form-signin mt-5' src='https://png.pngtree.com/png-vector/20230816/ourmid/pngtree-this-is-a-star-wars-chewy-face-decal-sticker-clipart-vector-png-image_6977660.png' ></img>
             </div>
-            <form className="row g-3 mx-4 mt-2">
+            <form className="row g-3 mx-4 mt-2" onSubmit={(e) => {
+                e.preventDefault()
+                actions.addUser()
+                e.target.reset()
+            }}>
                 <div className="form-floating mb-3">
-                    <input type="text" className="form-control input-signin" id="floatingInput" placeholder='Name' />
-                    <label className='text-warning' for="floatingInput">Name</label>
+                    <input
+                        type="text"
+                        className="form-control input-signin"
+                        id="name"
+                        name='name'
+                        onChange={actions.handleChange}
+                        placeholder='Name'
+                    />
+                    <label className='text-warning' htmlFor="name">Name</label>
                 </div>
                 <div className="form-floating mb-3">
-                    <input type="text" className="form-control input-signin" id="floatingInput" placeholder='Phone' />
-                    <label className='text-warning' for="floatingInput">Phone</label>
+                    <input
+                        type="text"
+                        className="form-control input-signin"
+                        id="phone"
+                        name='phone'
+                        onChange={actions.handleChange}
+                        placeholder='Phone'
+                    />
+                    <label className='text-warning' htmlFor="phone">Phone</label>
                 </div>
                 <div className="form-floating mb-3">
-                    <input type="email" className="form-control input-signin" id="floatingInput" placeholder="name@example.com" />
-                    <label className='text-warning' for="floatingInput">Email address</label>
+                    <input
+                        type="email"
+                        className="form-control input-signin"
+                        id="email"
+                        name='email'
+                        onChange={actions.handleChange}
+                        placeholder="name@example.com" />
+                    <label className='text-warning' htmlFor="email">Email address</label>
                 </div>
                 <div className="form-floating d-flex position-relative align-items-center">
-                    <input type={showPass ? "text" : "password"} className="form-control input-signin" id="floatingPassword" placeholder="Password" />
+                    <input
+                        type={showPass ? "text" : "password"}
+                        className="form-control input-signin"
+                        id="password"
+                        name='password'
+                        onChange={actions.handleChange}
+                        placeholder="Password"
+                    />
                     <div onClick={() => { setShowPass(!showPass) }}>
                         {showPass ?
                             <i className="fa-solid fa-eye text-yellow"></i> :
-                            <i class="fa-solid fa-eye-slash text-yellow"></i>
+                            <i className="fa-solid fa-eye-slash text-yellow"></i>
                         }
                     </div>
-                    <label className="text-warning" for="floatingPassword">Password</label>
+                    <label className="text-warning" htmlFor="password">Password</label>
                 </div>
                 <div className="form-floating d-flex position-relative align-items-center">
-                    <input type={showConfirmPass ? "text" : "password"} className="form-control input-signin" id="floatingPassword" placeholder="Password" />
+                    <input
+                        type={showConfirmPass ? "text" : "password"}
+                        className="form-control input-signin"
+                        id="confirmPassword"
+                        name='confirmPassword'
+                        onChange={actions.handleChange}
+                        placeholder="Password"
+                    />
                     <div onClick={() => { setShowConfirmPass(!showConfirmPass) }}>
                         {showConfirmPass ?
                             <i className="fa-solid fa-eye text-yellow"></i> :
-                            <i class="fa-solid fa-eye-slash text-yellow"></i>
+                            <i className="fa-solid fa-eye-slash text-yellow"></i>
                         }
                     </div>
-                    <label className="text-warning" for="floatingPassword">Confirm Password</label>
+                    <label className="text-warning" htmlFor="confirmPassword">Confirm Password</label>
                 </div>
                 <div className='text-center mb-2'>
-                    <button className="btn btn-yellow mt-2" type="submit" onClick={() => {
-                        Swal.fire({
-                            title: "Welcome Back",
-                            timer: 2000,
-                            padding: "2em",
-                            color: "#FFC107",
-                            showConfirmButton: false,
-                            background: `#000000
-                            url("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7ce55e30-e602-4e3d-b05a-d2a7a0fa49d8/daqj3gl-9a94472a-1945-4acd-ac69-b8eba9806db9.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzdjZTU1ZTMwLWU2MDItNGUzZC1iMDVhLWQyYTdhMGZhNDlkOFwvZGFxajNnbC05YTk0NDcyYS0xOTQ1LTRhY2QtYWM2OS1iOGViYTk4MDZkYjkucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.pSd9UXJUti2afeziH1UqbQzFGAnKPSWtnjukxIAqnO8") 
-                            no-repeat
-                            `,
-                            showclass: {
-                                popup: `animate__animated
-                                animate__fadeInUp
-                                animate__faster`
-                            },
-                            hideClass: {
-                                popup: `animate__animated
-                                animate__fadeOutDown
-                                animate__faster`
-                            },
-                            backdrop: `
-                            rgba(0,0,123,0.4)
-                            url("https://media.tenor.com/ONv6f0zBNFYAAAAj/hugging-grogu.gif")
-                            right top 
-                            no-repeat
-                            `
-                        })
-                    }
-                    }>SignIn
-                    </button>
+                    <button className="btn btn-yellow mt-2" type="submit">SignIn</button>
                     <div className='text-end text-yellow'>
                         <Link to="/login" className='text-decoration-none text-yellow'>
-                            <span>or LogIn <i class="fa-solid fa-shuttle-space"></i></span>
+                            <span>or LogIn <i className="fa-solid fa-shuttle-space"></i></span>
                         </Link>
                     </div>
                 </div>
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -14,7 +14,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 			userLogin: JSON.parse(localStorage.getItem("userLogin")) == undefined ? {} : JSON.parse(localStorage.getItem("userLogin")),
 			isLoggedIn: JSON.parse(localStorage.getItem("isLoggedIn")) == undefined ? false : JSON.parse(localStorage.getItem("isLoggedIn")),
-			emailSent : false,
+			emailSent: false,
 
 			phone: null,
 			email: null,
@@ -157,45 +157,45 @@ const getState = ({ getStore, getActions, setStore }) => {
 				setStore({ favoritesStarships: [] })
 				localStorage.clear();
 			},
-			resetPassword : async () =>{
-				try{
-				const store = getStore()
-				const actions = getActions()
+			resetPassword: async () => {
+				try {
+					const store = getStore()
+					const actions = getActions()
 
-				const response = await fetch(process.env.BACKEND_URL + `/resetPassword/${store.email}`,{
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json'
-					}
-				})
-				const result = await response.json()
+					const response = await fetch(process.env.BACKEND_URL + `/resetPassword/${store.email}`, {
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json'
+						}
+					})
+					const result = await response.json()
 
-				if (result.msg == "ok"){
-					setStore({ emailSent: true})
-					Swal.fire({
-						title: "Do it!!!",
-						text: `Email Sent`,
-						timer: 3000,
-						padding: "2em",
-						color: "#FFC107",
-						showConfirmButton: false,
-						background: `#000000
+					if (result.msg == "ok") {
+						setStore({ emailSent: true })
+						Swal.fire({
+							title: "Do it!!!",
+							text: `Email Sent`,
+							timer: 3000,
+							padding: "2em",
+							color: "#FFC107",
+							showConfirmButton: false,
+							background: `#000000
 						url("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7ce55e30-e602-4e3d-b05a-d2a7a0fa49d8/daqj3gl-9a94472a-1945-4acd-ac69-b8eba9806db9.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzdjZTU1ZTMwLWU2MDItNGUzZC1iMDVhLWQyYTdhMGZhNDlkOFwvZGFxajNnbC05YTk0NDcyYS0xOTQ1LTRhY2QtYWM2OS1iOGViYTk4MDZkYjkucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.pSd9UXJUti2afeziH1UqbQzFGAnKPSWtnjukxIAqnO8") 
 						no-repeat`,
-						backdrop: `rgba(0,0,123,0.4)
+							backdrop: `rgba(0,0,123,0.4)
 						url("https://media0.giphy.com/media/sFawvbjwFzgZdAa7K3/giphy.gif?cid=6c09b952c62nytmzsvs25flf4kqhqcob45kopogktsqn4xx9&ep=v1_stickers_related&rid=giphy.gif&ct=s")
 						right top 
 						no-repeat`
-					})
-					actions.clearStore()
-					setStore({emailSent: false})
-				}else{
-					actions.showSwalError(result.message)
-					actions.clearStore()
+						})
+						actions.clearStore()
+						setStore({ emailSent: false })
+					} else {
+						actions.showSwalError(result.message)
+						actions.clearStore()
+					}
+				} catch (error) {
+					console.log(error + " Error in resetPassword")
 				}
-			}catch (error) {
-				console.log(error + " Error in resetPassword")
-			}
 			},
 
 			//<---------------------------User------------------------------>//
@@ -214,10 +214,10 @@ const getState = ({ getStore, getActions, setStore }) => {
 					})
 					const result = await response.json()
 					console.log(result);
-					if (result.msg == "ok"){
+					if (result.msg == "ok") {
 						setStore({ userLogin: result.User })
 						localStorage.setItem("userLogin", JSON.stringify(result.User))
-					}else{
+					} else {
 						actions.showSwalError(result.message)
 					}
 
@@ -280,6 +280,65 @@ const getState = ({ getStore, getActions, setStore }) => {
 				} catch (error) {
 					console.log(error + " Error in editUser")
 				}
+			},
+			addUser: async () => {
+				try {
+					const store = getStore()
+					const actions = getActions()
+
+					const user = {}
+
+					if (store.password != null && store.confirmPassword != null) {
+						if (store.password == store.confirmPassword) {
+							user.password = store.password
+						} else {
+							actions.showSwalError("The password don't match")
+						}
+					}
+					if (store.name != null) {
+						user.name = store.name
+					}
+					if (store.phone != null) {
+						user.phone = store.phone
+					}
+					if (store.email != null) {
+						user.email = store.email
+					}
+
+					const response = await fetch(process.env.BACKEND_URL + '/user', {
+						method: 'POST',
+						body: JSON.stringify(user),
+						headers: {
+							'Content-Type': 'application/json'
+						}
+					})
+					const result = await  response.json()
+					if (result.msg == "ok") {
+						Swal.fire({
+							title: "Do it!!!",
+							text: `Welcome ${result.User.name}`,
+							timer: 3000,
+							padding: "2em",
+							color: "#FFC107",
+							showConfirmButton: false,
+							background: `#000000
+							url("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7ce55e30-e602-4e3d-b05a-d2a7a0fa49d8/daqj3gl-9a94472a-1945-4acd-ac69-b8eba9806db9.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzdjZTU1ZTMwLWU2MDItNGUzZC1iMDVhLWQyYTdhMGZhNDlkOFwvZGFxajNnbC05YTk0NDcyYS0xOTQ1LTRhY2QtYWM2OS1iOGViYTk4MDZkYjkucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.pSd9UXJUti2afeziH1UqbQzFGAnKPSWtnjukxIAqnO8") 
+							no-repeat`,
+							backdrop: `rgba(0,0,123,0.4)
+							url("https://64.media.tumblr.com/44c8c892b4bc22685344a2cedf044b8a/0646d51ff4487ec9-77/s500x750/4c9e28ee49d31b551f91d0b82cca8d67b6a03c10.gif")
+							right top 
+							no-repeat`
+						})
+						actions.clearStore()
+					}
+					else {
+						actions.showSwalError(result.message)
+						actions.clearStore()
+					}
+				} catch (error) {
+					console.log(error + " Error in addUser")
+				}
+
 			},
 
 			//<---------------------------People------------------------------>//
